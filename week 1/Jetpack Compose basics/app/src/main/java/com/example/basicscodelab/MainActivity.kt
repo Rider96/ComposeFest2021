d14852576f5829b3f1c0b8df604aa04af22a0d94
KimYongYeon <duszld604@gmail.com>
2021-11-23 14:43:34 +0900

    week 1 : UI 조정

--- week 1/Jetpack Compose basics/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ week 1/Jetpack Compose basics/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -10,6 +10,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,8 +29,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
 }
 
 @Preview(showBackground = true)
